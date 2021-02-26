312390713bd950778ea853eedd170f4f4b0e1fdd
anagha <anagharaj95@gmail.com>
2021-02-26 22:57:46 +0530

    Added the code

---
+++ server.js
@@ -0,0 +1,14 @@
+module.exports = {
+    RandomChar: function (num) {
+        var string = "abcdefghijklmnopqrstuvwxyz0123456789";
+        var str = "";
+        var i = 0;
+
+        while (i < num) {
+            str += string.charAt(Math.floor(Math.random() * string.length )); // * string.length
+            // console.log(str);
+            i++;
+        }
+        return str;
+    }
+}
